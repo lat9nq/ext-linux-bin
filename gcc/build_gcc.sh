ab35c40611d9d8e0dbc73cf2c1d45fa9cd2ca8bd
lat9nq <lat9nq@gmail.com>
2022-05-03 23:07:26 -0400

    gcc: Script corrections

--- gcc/build_gcc.sh
+++ gcc/build_gcc.sh
@@ -3,7 +3,8 @@
 # This script is meant to make it easy to build GCC using a Docker container.
 
 # Run this from the same directory as gcc source directory
-# Downloaded source archive must come from https://www.boost.org/
+# Recommended to clone GCC with:
+#    git clone --depth 1 -b "releases/gcc-11.3.0" git://gcc.gnu.org/git/gcc.git
 
 THIS=$(readlink -e $0)
 USER_ID=`id -u`
@@ -12,7 +13,7 @@ VERSION=11.3.0
 
 mkdir -p gcc-$VERSION
 
-#~ docker run -v $(pwd):/src -w /src -u root -t debian:test /bin/bash /src/docker.sh $VERSION
+docker run -v $(pwd):/src -w /src -u root -t debian:test /bin/bash /src/docker.sh $VERSION
 
 cp -v $THIS gcc-$VERSION/
 tar cv gcc-$VERSION | xz -T0 -c | split --bytes=90MB - gcc-$VERSION.tar.xz.
--- gcc/docker.sh
+++ gcc/docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+THIS=$(readlink -e $0)
+
 mkdir build
 cd build
 /src/gcc/configure --enable-languages=c,c++ --disable-multilib
@@ -7,3 +9,4 @@ make -j$(nproc)
 
 mkdir gcc-$1
 make -j$(nproc) install DESTDIR=/src/gcc-$1
+cp $THIS /src/gcc-$1
